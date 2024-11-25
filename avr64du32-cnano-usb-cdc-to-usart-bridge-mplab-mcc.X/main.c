adb2854ff96a26b92e44250b8e05882c9536d731
Henrik Moe Arnesen <HenrikMoe.Arnesen@microchip.com>
2024-11-25 11:20:34 +0100

    MPAE-18068: Update example to include power handling, interrupt driven USART and code cleanup

--- avr64du32-cnano-usb-cdc-to-usart-bridge-mplab-mcc.X/main.c
+++ avr64du32-cnano-usb-cdc-to-usart-bridge-mplab-mcc.X/main.c
@@ -2,10 +2,9 @@
  * MAIN
  * @file main.c
  * @defgroup main MAIN
- * @brief This file contain an example for running a USB CDC to USART bridge for AVR DU
- * @version MAIN Driver Version 1.0.0
+ * @brief This file contain an example for running a USART <-> CDC bridge over USB for AVR DU
+ * @version MAIN Driver Version 1.0.2
  */
-
 /*
 � [2024] Microchip Technology Inc. and its subsidiaries.
 
@@ -27,30 +26,53 @@
     THIS SOFTWARE.
 */
 
+// MCC includes
 #include "mcc_generated_files/system/system.h"
 #include "usb_cdc.h"
 #include "usb_cdc_virtual_serial_port.h"
-#include "usart_manager.h"
+
+// Standard library includes
+#include <stdbool.h>
+#include <util/delay.h>
+
+// Number of consecutive equal AC measurements before power is seen as stable
+#define AC_MEASUREMENT_STABLE_COUNT 5U
 
 // Status variables
 volatile RETURN_CODE_t status = SUCCESS;
 volatile CDC_RETURN_CODE_t cdcStatus = CDC_SUCCESS;
-volatile USART_RETURN_CODE_t usartStatus = USART_SUCCESS;
 
-// Data variables
-uint8_t usartData;
-uint8_t cdcData;
+// Interrupt variable
+volatile bool vbusFlag = false;
+
+// Private function declarations
+void USB_ConnectionHandler(void);
+void USB_PowerHandler(void);
 
 int main(void)
 {
+    // Data variables
+    uint8_t usartData;
+    uint8_t cdcData;
+
     SYSTEM_Initialize();
+
+    // Set callbacks
+    RTC_SetPITIsrCallback(USB_PowerHandler);
 
     while (1)
     {
-        // Running USB handle
-        status = USBDevice_Handle();
+        USB_ConnectionHandler();
 
-        if (SUCCESS == status)
+        if (SUCCESS != status)
+        {
+            while (1)
+            {
+                LED_Toggle();
+                _delay_ms(500);
+            }
+        }
+        else
         {
             // Tests if DTE is set and USB ready for transfer
             if (true == USB_CDCDataTerminalReady())
@@ -59,59 +81,105 @@ int main(void)
                 if (false == USB_CDCTxBusy())
                 {
                     // Retrieves USART data if available and writes to CDC
-                    usartStatus = USART_RxBufferRead(&usartData);
-                    if (USART_SUCCESS == usartStatus)
+                    if (USART1_IsRxReady())
                     {
-                        cdcStatus = USB_CDCWrite(usartData);
+                        usartData = USART1_Read();
+                        USB_CDCWrite(usartData);
                     }
-                    else
-                    {
-                        usartStatus = USART_SUCCESS; // No USART data available
-                    }
-                }
-                else
-                {
-                    ; // CDC TX buffer full, retry on next iteration
                 }
 
                 // Checks that USART buffer is not full
-                if (false == USART_TxBufferFull())
+                if (USART1_IsTxReady())
                 {
                     // Retrieves CDC data if available and writes to USART
                     cdcStatus = USB_CDCRead(&cdcData);
                     if (CDC_SUCCESS == cdcStatus)
                     {
-                        usartStatus = USART_TxBufferWrite(cdcData);
+                        USART1_Write(cdcData);
                     }
-                    else
-                    {
-                        cdcStatus = CDC_SUCCESS; // No CDC data available
-                    }
-                }
-                else
-                {
-                    ; // USART TX buffer full, retry on next iteration
                 }
 
-                // Running CDC and USART handlers
+                // Running CDC Virtual Serial Port handler
                 status = USB_CDCVirtualSerialPortHandler();
-                if (SUCCESS == status && USART_SUCCESS == usartStatus)
-                {
-                    usartStatus = USART_Handler();
-                }
-                else
-                {
-                    ; // CDC Handler or previous USART Write failed, skipping USART Handler
-                }
             }
-            else
-            {
-                ; // No terminal session active
-            }
+        }
+    }
+}
+
+/**
+ * Routine that checks the connectivity of the USB peripheral and start/stops the USB driver when connected/disconnected
+ */
+void USB_ConnectionHandler(void)
+{
+    static volatile bool prevVbusState = false;
+    // Check if VBUS was true last check, indicating that USB was connected
+    if (prevVbusState == true)
+    {
+        // Handle USB Transfers
+        status = USBDevice_Handle();
+    }
+    // Get current status of VBUS
+    bool currentVbusState = vbusFlag;
+    // If changes to USB VBUS state
+    if (currentVbusState != prevVbusState)
+    {
+        // If USB has been connected
+        if (currentVbusState == true)
+        {
+            // Start USB operations
+            status = USB_Start();
+            LED_SetHigh();
         }
         else
         {
-            ; // USB handle error
+            // Stop USB operations
+            status = USB_Stop();
+            LED_SetLow();
+        }
+        // Save state
+        prevVbusState = currentVbusState;
+    }
+}
+
+/**
+ * Interrupt routine used to check VBUS status using the Real-Time Counter overflow interrupt and Analog Comparator
+ */
+void USB_PowerHandler(void)
+{
+    // Helper variables for RTC and AC
+    static volatile uint8_t acStateCounter = 0U;
+    static volatile uint8_t acPrevState = 0U;
+
+    uint8_t acCurrentState = AC0_Read(); // Saves the current state of the AC status register ('1' means above threshold)
+
+    // Makes sure that the AC state has been the same for a certain number of PIT interrupts
+    if (acStateCounter == AC_MEASUREMENT_STABLE_COUNT)
+    {
+        if (acCurrentState) // When CMPSTATE is high, the AC output is high which means the VBUS is above the threshold and the pull-up on data+ should be enabled.
+        {
+            vbusFlag = true;
+        }
+        else
+        {
+            vbusFlag = false;
+        }
+        acStateCounter++; // Increment the counter to not run start/stop multiple times without an actual state change
+    }
+    // Increments a counter if the AC has been in the same state for a certain number of PIT interrupts
+    else if (acCurrentState == acPrevState)
+    {
+        if (acStateCounter < AC_MEASUREMENT_STABLE_COUNT)
+        {
+            acStateCounter++;
+        }
+        else
+        {
+            ; // Stops counting if the AC has been in the same state for a while.
         }
     }
+    else
+    {
+        acStateCounter = 0; // Resets the PIT counter if a state change has occurred.
+    }
+    acPrevState = acCurrentState;
 }
